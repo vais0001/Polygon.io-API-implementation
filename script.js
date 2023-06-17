1e178ee77bb89844e4fa196259f0d53b682c1bab
vais0001 <vais0001@hz.nl>
2023-06-17 12:39:26 +0200

    Added the code for the API

---
+++ script.js
@@ -0,0 +1,23 @@
+function getStockData() {
+    const stockInput = document.getElementById("stockInput");
+    const stockSymbol = stockInput.value;
+    const apiKey = "YOUR API KEY"; //Replace the placeholder with your own API key
+    const url = `https://api.polygon.io/v1/open-close/${stockSymbol}/2023-01-09?adjusted=true&apiKey=${apiKey}`;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const stockData = document.getElementById("stockData");
+        stockData.innerHTML = `
+          <h2>${stockSymbol} Stock Data</h2>
+          <p>Open Price: $${data.open}</p>
+          <p>Close Price: $${data.close}</p>
+          <p>High Price: $${data.high}</p>
+          <p>Low Price: $${data.low}</p>
+        `;
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+  }
+  
